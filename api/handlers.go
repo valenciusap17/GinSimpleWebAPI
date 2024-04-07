4a5f2812dc646852524f399c598bfae722b98c41
Valencius Apriady <v.apriady@gmail.com>
2024-04-07 20:34:23 +0700

    Add http method handle

--- api/handlers.go
+++ api/handlers.go
@@ -10,116 +10,141 @@ import (
 )
 
 func (s *Server) HandlePostUser(w http.ResponseWriter, r *http.Request) {
-	queryData := new(models.CreateUserRequest)
-	if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
-		log.Fatal(err)
-		return
-	}
-	respData, err := s.CreateUser(*queryData)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	if r.Method == "POST" {
+		queryData := new(models.CreateUserRequest)
+		if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
+			log.Fatal(err)
+			return
+		}
+		respData, err := s.CreateUser(*queryData)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	fmt.Println(respData)
+		fmt.Println(respData)
 
-	jsonData, err := json.Marshal(respData)
-	if err != nil {
-		log.Fatal(err)
-		return
+		jsonData, err := json.Marshal(respData)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(jsonData)
 	}
-
-	w.Header().Set("Vontent-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(jsonData)
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
 
 func (s *Server) HandleGetAllUSer(w http.ResponseWriter, r *http.Request) {
-	response, err := s.GetAllUser()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	if r.Method == "GET" {
+		response, err := s.GetAllUser()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		jsonData, err := json.Marshal(response)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+	}
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
 
 func (s *Server) HandleGetUSerById(w http.ResponseWriter, r *http.Request) {
-	id := &models.UserIdRequest{}
+	if r.Method == "GET" {
+		id := &models.UserIdRequest{}
 
-	if err := json.NewDecoder(r.Body).Decode(id); err != nil {
-		log.Fatal("inkah? ", err)
-		return
+		if err := json.NewDecoder(r.Body).Decode(id); err != nil {
+			log.Fatal("inkah? ", err)
+			return
+		}
+
+		response, err := s.GetUserById(&id.Id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		jsonData, err := json.Marshal(response)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
 	}
-
-	response, err := s.GetUserById(&id.Id)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "aplication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
 
 func (s *Server) HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
-	queryData := &models.UpdateUserRequest{}
+	if r.Method == "PUT" {
+		queryData := &models.UpdateUserRequest{}
 
-	if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
-		log.Fatal(err)
-		return
-	}
-	response, err := s.UpdateUser(&queryData.UserData, &queryData.Id)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+		if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
+			log.Fatal(err)
+			return
+		}
+		response, err := s.UpdateUser(&queryData.UserData, &queryData.Id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+		jsonData, err := json.Marshal(response)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+	}
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
 
 func (s *Server) HandleDeleteUser(w http.ResponseWriter, r *http.Request) {
-	queryData := new(models.UserIdRequest)
+	if r.Method == "DELETE" {
+		queryData := new(models.UserIdRequest)
 
-	if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
-		log.Fatal(err)
-		return
-	}
-	response, err := s.DeleteUser(&queryData.Id)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+		if err := json.NewDecoder(r.Body).Decode(queryData); err != nil {
+			log.Fatal(err)
+			return
+		}
+		response, err := s.DeleteUser(&queryData.Id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+		jsonData, err := json.Marshal(response)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+	}
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
